8f58780174149c1b4777c301210abddaca4fcc06
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-02-03 16:49:41 +0200

    fix: erroe message when no such country

--- src/index.js
+++ src/index.js
@@ -18,15 +18,15 @@ function onInput(event) {
     if (!countryName) {
         countryList.innerHTML = '';
         countryInfo.innerHTML = '';
-        // throw new Error('Error fetching data');
+        throw new Error();
     };
 
     fetchCountries(countryName)
         .then(renderMarkup)
-        .catch(onError);
+        .catch(onFetchError)
 }
 
-function onError() {
+function onFetchError(Error) {
     countryList.innerHTML = '';
     countryInfo.innerHTML = '';
     Notify.failure('Oops, there is no country with that name');
@@ -34,26 +34,24 @@ function onError() {
 
 function renderMarkup(countries) {
 
-    if (countries.length <= 1) {
+    if (countries.length === 1) {
         const markup = countries.map((country) => {
             const languages = Object.values(country.languages).join(', ');
             country.languages = languages;
-            return countryInfoMarkup(country);
-        })
-            .join('');
+            return countryInfoMarkup(country)
+        }).join('');
         countryList.innerHTML = '';
         countryInfo.innerHTML = markup;
     } else
-        if (countries.length <= 10) {
-            const markup = countries.map((country) => {
-                return countryItemMarkup(country)
-            })
-                .join('');
-            countryList.innerHTML = markup;
-            countryInfo.innerHTML = '';
-        } else {
+        if (countries.length > 10) {
             countryList.innerHTML = '';
             countryInfo.innerHTML = '';
             Notify.success('Too many matches found. Please enter a more specific name.')
+        } else {
+            const markup = countries.map((country) => {
+                return countryItemMarkup(country)
+            }).join('');
+            countryList.innerHTML = markup;
+            countryInfo.innerHTML = '';
         }
 }
